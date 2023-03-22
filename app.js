17f867e369279cf75a647f110fc0d42c83328f83
kayahankaya <kayahankaya@icloud.com>
2023-03-22 13:58:57 +0100

    add reviews as simple

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError')
-const { campgroundSchema } = require('./schemas');
-
+const { campgroundSchema, reviewSchema } = require('./schemas');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
     //useCreateIndex: true,
@@ -40,6 +40,16 @@ const validateCampground = (req,res,next) => {
     }
 }
 
+const validateReview = (req,res,next) => {
+    const {error} = reviewSchema.validate(req.body);
+    if (error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg,400)
+    } else {
+        next();
+    }
+}
+
 app.get('/', (req, res) => {
     res.render('home')
 });
@@ -61,7 +71,8 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 }))
 
 app.get('/campgrounds/:id', catchAsync(async (req, res,) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    console.log(campground)
     res.render('campgrounds/show', { campground });
 }));
 
@@ -82,6 +93,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }))
 
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req,res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
 app.all('*',(req,res,next) => {
     next(new ExpressError("Page Not Found", 404))
 })
@@ -95,3 +115,4 @@ app.use((err,req,res,next) => {
 app.listen(3000, () => {
     console.log('Serving on port 3000')
 })
+
