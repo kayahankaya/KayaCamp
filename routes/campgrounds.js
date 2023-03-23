cf78a8f2257937288976d18829ea953b20077a20
kayahankaya <kayahankaya@icloud.com>
2023-03-23 16:36:58 +0100

    add error flashes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -35,11 +35,19 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 
 router.get('/:id', catchAsync(async (req, res,) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if(!campground){
+        req.flash('error','Cannot find that campground!')
+        res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', { campground });
 }));
 
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
+    if(!campground){
+        req.flash('error','Cannot find that campground!')
+        res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', { campground });
 }))
 
@@ -53,6 +61,7 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) => {
 router.delete('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground!');
     res.redirect('/campgrounds');
 }))
 
