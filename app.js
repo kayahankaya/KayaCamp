b0b3906eea63d73d4ca4a1563dd4ed33118d7f93
kayahankaya <kayahankaya@icloud.com>
2023-03-24 11:43:19 +0100

    add passport package

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy  = require('passport-local');
+const User = require('./models/user')
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
 
@@ -44,6 +47,12 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User)
 
 app.use((req,res,next) => {
     res.locals.success = req.flash('success')
