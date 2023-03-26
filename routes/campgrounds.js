1599164a64f7bf24ce829d8e78d54d4ee3cbb4f7
kayahankaya <kayahankaya@icloud.com>
2023-03-26 18:52:59 +0200

    add cloudinary to upload multiple images

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-const campgrounds = require('../controllers/campgrounds')
+const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn , isAuthor, validateCampground } = require('../middleware');
 const { populate } = require('../models/campground');
-const Campground = require('../models/campground')
+const Campground = require('../models/campground');
+const multer = require('multer');
+const {storage} = require('../cloudinary');
+const upload = multer({storage});
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(isLoggedIn, validateCampground, upload.array('image'), catchAsync(campgrounds.createCampground))
+
 
 router.route('/new')
     .get(isLoggedIn, campgrounds.renderNewForm)
