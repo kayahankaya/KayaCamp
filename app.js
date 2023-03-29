6cff504c0b84c214863851290c74eaf0e13f9eb2
kayahankaya <kayahankaya@icloud.com>
2023-03-29 22:28:59 +0200

    fix mongodb connection error

--- app.js
+++ app.js
@@ -22,11 +22,10 @@ const MongoStore = require("connect-mongo");
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
